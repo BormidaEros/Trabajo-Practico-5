2cfd47e31ee4b315f6ff58775d47fe165287f8e3
BormidaEros <eros.bormida.epet20@gmail.com>
2022-10-06 11:25:26 -0300

    cargo los puntos del trabajo practico

---
+++ practica.js
@@ -0,0 +1,26 @@
+function suma(a, b){
+    return a + b;
+}
+const resultadoS = suma (5, 10);
+console.log(resultadoS);
+function resta(c, d){
+    return c - d;
+}
+const resultadorR = resta (10, 3);
+console.log(resultadorR);
+function multi(a, b){
+    return a * b;
+}
+const resultadoMul = multi (5, 10);
+console.log(resultadoMul);
+function dividir (a, b){
+    return a / b;
+}
+const resultadoDiv = dividir (12, 3);
+console.log(resultadoDiv);
+
+
+arregloNumeros=[1,2,3,4,5];
+for ( i = 0 ; i < arregloNumeros.length ; i ++ ){
+ console.log(arregloNumeros[i]);
+}
